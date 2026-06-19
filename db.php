d426f6213db0dfd53793d8aaf652dff1662e74ce
Amsha67 <eric.schmolzlin@gmail.com>
2026-06-19 13:27:58 +0200

    bug

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 try {
-   $pdo = new PDO("mysql:host=localhost;dbname=gestion_collections;charset=utf8", "root", "root");
+   $pdo = new PDO("mysql:host=127.0.0.1;port=8889;dbname=gestion_collections;charset=utf8", "root", "root");
    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(Exception $e) {
+} catch (Exception $e) {
    die("Erreur : " . $e->getMessage());
 }
